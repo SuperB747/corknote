9797aede4aa2ef7568449b1fbfe773a2ba7fabb1
Brian Son <briansonjb@gmail.com>
2025-06-09 13:06:18 -0700

    render.com 으로 올리기 위해 최적화화

--- client/src/firebase/config.ts
+++ client/src/firebase/config.ts
@@ -4,12 +4,12 @@ import { getFirestore, connectFirestoreEmulator, Firestore } from 'firebase/fire
 import { getStorage, connectStorageEmulator, FirebaseStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "demo-key",
-  authDomain: "localhost",
-  projectId: "corknote",
-  storageBucket: "corknote-local.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "demo-app-id"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID as string,
 };
 
 class FirebaseService {
