5cd82a4728791e7faeee58289af9c55700cde7c7
Brian Son <briansonjb@gmail.com>
2025-06-11 22:18:30 -0700

    노트 사이드바 위로

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="h-screen flex overflow-hidden">
+    <div className="h-screen flex overflow-visible">
       <Sidebar />
       <main className="flex-1 relative z-10">
         {children}
